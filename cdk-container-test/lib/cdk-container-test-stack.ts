b464ee5c14b0f0811b6a555cf62fe0459dfe9799
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-30 20:46:14 +0900

    Add: CDKでCloudFrontディストリビューションを作成するコードを追加

--- cdk-container-test/lib/cdk-container-test-stack.ts
+++ cdk-container-test/lib/cdk-container-test-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class CdkContainerTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3バケットの作成
+    const myBucket = new s3.Bucket(this, "myBucket", {
+      bucketName: "yuma-cdk-container-test",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkContainerTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CloudFrontディストリビューションの作成
+    const distribution = new cloudfront.Distribution(this, "myDist", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(myBucket),
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+      },
+    });
+
+    // S3バケットに画像をデプロイ
+    new s3deploy.BucketDeployment(this, 'DeployImage', {
+      sources: [s3deploy.Source.asset('/虹色孔雀.jpg')],
+      destinationBucket: myBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    // CloudFrontのURLを出力
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.domainName,
+    });
   }
 }
